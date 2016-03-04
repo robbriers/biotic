cc0966620261fb127dbe206f0cd182fae9896ec6
Rob Briers <r.briers@napier.ac.uk>
2016-03-04 17:50:29 +0000

    updating docs for datasets

---
+++ R/BMWPtab.r
@@ -0,0 +1,8 @@
+#' @name BMWPtab
+#' @title Table of biotic index scores
+#' @description Full list of scores used in calculations
+#' @keywords datasets
+#' @docType data
+#' @format DataFrame
+#' @author Rob Briers, 2016
+"BMWPtab"
--- R/almond.r
+++ R/almond.r
@@ -10,12 +10,3 @@
 #' @format DataFrame
 #' @author David Lawson, 2015
 "almond"
-
-#' @name BMWPtab
-#' @title Table of biotic index scores
-#' @description Full list of scores used in calculations
-#' @keywords datasets
-#' @docType data
-#' @format DataFrame
-#' @author Rob Briers, 2016
-"BMWPtab"
